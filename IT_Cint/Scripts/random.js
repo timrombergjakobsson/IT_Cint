e3661ffc03a9eaf08bb832637cebd5502f34e6a4
unknown <exjobb2011@.stockholm.cint.com>
2011-04-29 09:46:29 +0200

    extracted the navigator geolocation code to an external file if we would like to use it in the future

---
+++ IT_Cint/Scripts/random.js
@@ -0,0 +1,57 @@
+﻿/*var count = 0;
+(function($) {
+$('#counter').html(function(i, val) {
+$.ajax({
+url: '/path/to/script/',
+type: 'GET',
+data: {increment: true},
+success: function() { alert('Request has returned') }
+});
+return +val+1;
+});
+}*/
+
+
+/*function getBrowserLang() {
+if (navigator.geolocation) {
+browserSupportFlag = true;
+navigator.geolocation.getCurrentPosition(function (position) {
+initialLocation = new Google.maps.Latlng(position.coords.latitude, position.coords.longtitude);
+map.setCenter(initialLocation);
+}, function () {
+handleNoGeoLocation(browserSupportFlag);
+});
+
+//Try Google Geo Gears Geolocation
+}
+else if (google.gears) {
+browserSupportFlag = true;
+var geo = google.gears.factory.create('beta.geolocation');
+geo.getCurrenPosition(function (position) {
+initialLocation = new google.maps.LatLng(position.latitude, position.longitude);
+map.setCenter(initialLocation);
+}, function () {
+handleNoGeoLocation(browserSupportFlag);
+});
+// Browser does'nt support GeoLocation
+} else {
+browserSupportFlag = false;
+handleNoGeoLocation(browserSupportFlag);
+}
+
+function handleNoGeoLocation(errorFlag) {
+if (errorFlag == true) {
+alert("Geolocation service failed");
+initialLocation == newyork;
+} else {
+alert("Your browser does not support geolocation. We've placed you in Siberia.");
+initialLocation == siberia;
+}
+map.setCenter(initialLocation);
+
+return (navigator.language || navigator.userLanguage);
+}
+
+map.setCenter(newLatLng);
+map.setZoom(zoom);
+});*/
